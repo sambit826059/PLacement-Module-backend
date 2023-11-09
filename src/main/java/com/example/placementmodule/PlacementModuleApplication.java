b3689a38dcbc84cd5bb82abcba46a892966f1fc5
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-09 14:31:28 +0530

    making code readable

--- src/main/java/com/example/placementmodule/PlacementModuleApplication.java
+++ src/main/java/com/example/placementmodule/PlacementModuleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlacementModuleApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PlacementModuleApplication.class, args);
     }
 
