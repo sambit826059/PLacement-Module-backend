82c6f22c931dc781525dc6b67444ddf16f2ebc4a
sambit826059 <sambitmohanty1947@gmail.com>
2023-11-17 20:05:22 +0530

    application entity

---
+++ src/main/java/com/example/placementmodule/model/Application.java
@@ -0,0 +1,49 @@
+package com.example.placementmodule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long studentId; // Foreign key to link with the Student entity
+    private Long jobPostingId; // Foreign key to link with the JobPosting entity
+    private String status; // Pending, Accepted, Rejected, etc.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public Long getJobPostingId() {
+        return jobPostingId;
+    }
+
+    public void setJobPostingId(Long jobPostingId) {
+        this.jobPostingId = jobPostingId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
