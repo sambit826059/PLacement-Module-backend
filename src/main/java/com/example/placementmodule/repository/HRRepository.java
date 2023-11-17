4d6565ff8225c3cfa9c8f18be0b546accb12404e
soumen bhunia <sob99338@gmail.com>
2023-11-17 18:07:29 +0530

    HR repository

--- src/main/java/com/example/placementmodule/repository/HRRepository.java
+++ src/main/java/com/example/placementmodule/repository/HRRepository.java
@@ -1,2 +1,8 @@
-package com.example.placementmodule.repository;public interface HRRepository {
+package com.example.placementmodule.repository;
+
+import com.example.placementmodule.model.HR;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HRRepository extends JpaRepository<HR, Long> {
+    // You can add custom queries or methods if needed
 }
