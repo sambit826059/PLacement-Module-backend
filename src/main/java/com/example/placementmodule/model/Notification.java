9cff5a1ea219bfa840e8405445b7a7eddcd98fad
sambit826059 <sambitmohanty1947@gmail.com>
2023-11-18 15:58:03 +0530

    notification entity

--- src/main/java/com/example/placementmodule/model/Notification.java
+++ src/main/java/com/example/placementmodule/model/Notification.java
@@ -13,7 +13,37 @@ public class Notification {
 
     private Long userId; // Id of the user receiving the notification
     private String message;
-    private boolean read; // Indicates whether the notification has been read
+    private boolean isRead; // Indicates whether the notification has been read
 
-    // Constructors, getters, setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
 }
