2faa6d1073f3c507c0e71c94a842f5d237ecfaa0
sambit826059 <sambitmohanty1947@gmail.com>
2023-11-18 15:58:41 +0530

    notification repository

--- src/main/java/com/example/placementmodule/repository/NotificationRepository.java
+++ src/main/java/com/example/placementmodule/repository/NotificationRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUserIdAndRead(Long userId, boolean read);
-
+    List<Notification> findByUserIdAndIsRead(Long userId, boolean b);
 }
