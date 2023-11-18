0f1ffeaf45a273ad9a4a11830b7c3976cee98f6b
sambit826059 <sambitmohanty1947@gmail.com>
2023-11-18 17:26:22 +0530

    job type repository

--- src/main/java/com/example/placementmodule/repository/JobPostingRepository.java
+++ src/main/java/com/example/placementmodule/repository/JobPostingRepository.java
@@ -3,6 +3,10 @@ package com.example.placementmodule.repository;
 import com.example.placementmodule.model.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
+
+    List<JobPosting> findByEmploymentType(String employmentType);
 
 }
